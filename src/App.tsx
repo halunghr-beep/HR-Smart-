8c6a298799569524b0c3837af6dddbff5cf9dfc5
halunghr-beep <halunghr@gmail.com>
2026-03-19 02:04:53 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2461,8 +2461,6 @@ directToCeo: availableUsers.find(u => u.matricule === formData.employeeMatricule
                       Submit Request
                     </button>
                   </div>
-                      Cancel
-                    </button>
                     <button 
                       type="submit"
                       className="flex-[2] bg-indigo-600 text-white py-4 rounded-xl font-bold shadow-lg shadow-indigo-200 hover:bg-indigo-700 transition-all uppercase tracking-widest text-xs"
