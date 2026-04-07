f3750a7a8779040b921d9c0b87249c07498c12c8
halunghr-beep <halunghr@gmail.com>
2026-04-07 17:39:14 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,13 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.halung.hrsmart',
   appName: 'HR Smart',
   webDir: 'dist',
+  server: {
+    url: 'https://hr-smart.onrender.com',  // ← hena el sirr
+    cleartext: true,
+    androidScheme: 'https'
+  },
   android: {
     allowMixedContent: true
   }
 };
-
-export default config;
